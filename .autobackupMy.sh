78d63037540e1317d79878d7cc99661b252d4b9b
Deon Kim osX <dae.h.kim@mail.mcgill.ca>
2016-09-19 16:51:58 -0400

    unify .bashrc, no profile anymore

--- .autobackupMy.sh
+++ .autobackupMy.sh
@@ -2,11 +2,10 @@
 
 cp ~/.vimrc ~/runtime_config/.vimrc
 cp ~/.zshrc ~/runtime_config/.zshrc
-cp ~/.bash_profile ~/runtime_config/.bash_profile
+cp ~/.bashrc ~/runtime_config/.bashrc
 cp ~/.inputrc ~/runtime_config/.inputrc
 cp ~/.tmux.conf ~/runtime_config/.tmux.conf
 cp ~/.autobackupMy.sh ~/runtime_config/.autobackupMy.sh
-cp ~/.bundleUpdateVimMy.sh ~/runtime_config/.bundleUpdateVimMy.sh
 
 
 
