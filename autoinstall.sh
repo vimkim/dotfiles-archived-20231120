e39681be52062436cf4648374620face4ec654d0
dqgthb <deonquebec@gmail.com>
2016-09-19 19:54:02 -0400

    .bashrc for vim layout

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,3 +3,5 @@
 ln -s ~/runtime_config/.bashrc ~/.bashrc
 ln -s ~/runtime_config/.zshrc ~/.zshrc 
 ln -s ~/runtime_config/.vimrc ~/.vimrc
+ln -s ~/runtime_config/.inputrc ~/.inputrc
+ln -s ~/runtime_config/.tmux.conf ~/.tmux.conf
