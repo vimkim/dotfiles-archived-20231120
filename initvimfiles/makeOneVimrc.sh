00972006180f137392421e3270f58174cafbf97a
Deon Kim osX <dae.h.kim@mail.mcgill.ca>
2016-09-23 05:50:37 -0400

    create a simple sh script for creating one init.vim file

---
+++ initvimfiles/makeOneVimrc.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+cat ~/runtime_config/initvimfiles/vimtips.txt > oneInit.vim
+cat ~/runtime_config/initvimfiles/mappings.vimrc >> oneInit.vim
+cat ~/runtime_config/initvimfiles/plugins.vimrc >> oneInit.vim
+cat ~/runtime_config/initvimfiles/oneliner.vimrc >> oneInit.vim
+cat ~/runtime_config/initvimfiles/colorschemes.vimrc >> oneInit.vim
+cat ~/runtime_config/initvimfiles/misc.vimrc >> oneInit.vim
+
