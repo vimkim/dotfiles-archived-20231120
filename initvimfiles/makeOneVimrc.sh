6ed3707cad86ceb8e53242ab160e73b295052dce
dqgthb <deonquebec@gmail.com>
2016-09-29 15:48:01 -0400

    making one complete init.vim file

--- initvimfiles/makeOneVimrc.sh
+++ initvimfiles/makeOneVimrc.sh
@@ -1,9 +1,14 @@
 #! /bin/sh
 
-cat ~/runtime_config/initvimfiles/vimtips.txt > oneInit.vim
-cat ~/runtime_config/initvimfiles/mappings.vimrc >> oneInit.vim
-cat ~/runtime_config/initvimfiles/plugins.vimrc >> oneInit.vim
-cat ~/runtime_config/initvimfiles/oneliner.vimrc >> oneInit.vim
-cat ~/runtime_config/initvimfiles/colorschemes.vimrc >> oneInit.vim
-cat ~/runtime_config/initvimfiles/misc.vimrc >> oneInit.vim
+cat ~/runtime_config/initvimfiles/vimtips.txt > ~/runtime_config/initvimfiles/oneInit.vim
+cat ~/runtime_config/initvimfiles/mappings.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
+
+cat ~/runtime_config/initvimfiles/plugins.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
+
+cat ~/runtime_config/initvimfiles/oneliner.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
+
+cat ~/runtime_config/initvimfiles/colorschemes.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
+
+cat ~/runtime_config/initvimfiles/misc.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
+
 
