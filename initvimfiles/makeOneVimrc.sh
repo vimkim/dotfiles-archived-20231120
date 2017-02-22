45895520b91fd7b6a965e12c70f548dd3ea6ed84
dqgthb <deonquebec@gmail.com>
2017-02-22 05:59:26 -0500

    oneInit update : around 750 lines

--- initvimfiles/makeOneVimrc.sh
+++ initvimfiles/makeOneVimrc.sh
@@ -2,16 +2,11 @@
 
 cat ~/runtime_config/initvimfiles/vimtips.txt > ~/runtime_config/initvimfiles/oneInit.vim
 cat ~/runtime_config/initvimfiles/mappings.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-
 cat ~/runtime_config/initvimfiles/plugins.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-
+cat ~/runtime_config/initvimfiles/filetypes.vim >> ~/runtime_config/initvimfiles/oneInit.vim
+cat ~/runtime_config/initvimfiles/latex.vim >> ~/runtime_config/initvimfiles/oneInit.vim
 cat ~/runtime_config/initvimfiles/oneliner.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-
 cat ~/runtime_config/initvimfiles/colorschemes.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-
 cat ~/runtime_config/initvimfiles/misc.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-
 cat ~/runtime_config/initvimfiles/gistfile.vim >> ~/runtime_config/initvimfiles/oneInit.vim
-
-
 
