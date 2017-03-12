16b8ba51ed58ca791399413a27bca8f8196a0604
dqgthb <deonquebec@gmail.com>
2017-03-12 05:04:49 -0400

    as initvimfiles directory was renamed

--- nvim_initfiles/makeOneVimrc.sh
+++ nvim_initfiles/makeOneVimrc.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-cat ~/runtime_config/initvimfiles/vimtips.txt > ~/runtime_config/initvimfiles/oneInit.vim
-cat ~/runtime_config/initvimfiles/mappings.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-cat ~/runtime_config/initvimfiles/plugins.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-cat ~/runtime_config/initvimfiles/filetypes.vim >> ~/runtime_config/initvimfiles/oneInit.vim
-cat ~/runtime_config/initvimfiles/latex.vim >> ~/runtime_config/initvimfiles/oneInit.vim
-cat ~/runtime_config/initvimfiles/oneliner.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-cat ~/runtime_config/initvimfiles/colorschemes.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-cat ~/runtime_config/initvimfiles/misc.vimrc >> ~/runtime_config/initvimfiles/oneInit.vim
-cat ~/runtime_config/initvimfiles/gistfile.vim >> ~/runtime_config/initvimfiles/oneInit.vim
+cat ~/runtime_config/nvim_initfiles/vimtips.txt > ~/runtime_config/nvim_initfiles/IntegratedInit.vim
+cat ~/runtime_config/nvim_initfiles/mappings.vimrc >> ~/runtime_config/nvim_initfiles/IntegratedInit.vim
+cat ~/runtime_config/nvim_initfiles/plugins.vimrc >> ~/runtime_config/nvim_initfiles/IntegratedInit.vim
+cat ~/runtime_config/nvim_initfiles/filetypes.vim >> ~/runtime_config/nvim_initfiles/IntegratedInit.vim
+cat ~/runtime_config/nvim_initfiles/latex.vim >> ~/runtime_config/nvim_initfiles/IntegratedInit.vim
+cat ~/runtime_config/nvim_initfiles/oneliner.vimrc >> ~/runtime_config/nvim_initfiles/IntegratedInit.vim
+cat ~/runtime_config/nvim_initfiles/colorschemes.vimrc >> ~/runtime_config/nvim_initfiles/IntegratedInit.vim
+cat ~/runtime_config/nvim_initfiles/misc.vimrc >> ~/runtime_config/nvim_initfiles/IntegratedInit.vim
+cat ~/runtime_config/nvim_initfiles/gistfile.vim >> ~/runtime_config/nvim_initfiles/IntegratedInit.vim
 
