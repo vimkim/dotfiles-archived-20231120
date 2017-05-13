132c684b6603f088e4e3a8d7e9ec8aeb1f88011f
DQmacOS <deonquebec@gmail.com>
2017-05-13 14:28:43 +0900

    temp alias must not be version controlled at this moment

--- temp-aliaslist.sh
+++
@@ -1,6 +0,0 @@
-##### skim function
-alias skimpage="osascript ~/runtime_config/pdfpage.scpt"
-alias skimp="osascript ~/runtime_config/pdfpage.scpt"
-#####
-
-alias hw='skimp ~/Downloads/hs-homework.pdf 1'
