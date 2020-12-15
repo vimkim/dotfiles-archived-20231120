3404ca7efe7a9e4638aef381fa9868ae3b639605
dk <kimdhyungg@gmail.com>
2020-12-15 15:08:33 +0900

    .

--- auto/windows/automate.ps1
+++ auto/windows/automate.ps1
@@ -3,7 +3,7 @@
 # if error msg: cannot create a file when that file already exists
 # then: type taskschd.msc and delete duplicates
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "%homepath%\dkenv\runtime_config\auto\windows\auto.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File %homepath%\dkenv\runtime_config\auto\windows\auto.ps1 -WindowStyle Hidden"
 
 $trigger = New-ScheduledTaskTrigger -Once -at (get-date) -RepetitionInterval (New-TimeSpan -Minutes 1)
 
@@ -12,6 +12,6 @@ $trigger = New-ScheduledTaskTrigger -Once -at (get-date) -RepetitionInterval (Ne
 #Register-ScheduledTask auto -action $action -trigger $trigger -Settings $settings -Principal $principal
 Register-ScheduledTask auto -action $action -trigger $trigger
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "%homepath%\dkenv\runtime_config\auto\windows\autoBaekjoon.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File %homepath%\dkenv\runtime_config\auto\windows\autoBaekjoon.ps1 -WindowStyle Hidden"
 #Register-ScheduledTask autoBaekjoon -action $action -trigger $trigger -Settings $settings -Principal $principal
 Register-ScheduledTask autoBaekjoon -action $action -trigger $trigger
