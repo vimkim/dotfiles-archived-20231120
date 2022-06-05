194aa8bf08a4c5a764d25fac18bad86247c785f1
dqgthb <kimdhyungg@gmail.com>
2022-06-05 13:19:36 +0900

    cl

--- psh/desktop.ps1
+++ psh/desktop.ps1
@@ -65,3 +65,50 @@ function hw() {
 function ln ($target, $link) {
     new-item -path $link -itemtype SymbolicLink -value $target
 }
+
+### cl begin ###
+function _ls{Get-ChildItem -name @args -Force}
+
+Set-Alias l _ls
+
+Set-Alias -Name ls -Value _ls -Option AllScope
+
+function ll{Get-ChildItem}
+
+function _cd {
+    # for pipelines to work
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]
+        $_cd_paths
+    )
+    if ($null -eq $_cd_paths) {
+        Set-Location $home
+    }
+    else {
+        Set-Location $_cd_paths
+    }
+#if ($args.count -eq 0){
+#    Set-Location $home
+#}
+#else{
+#    Set-Location @args
+#}
+}
+
+Set-Alias -Name cd -Value _cd -Option AllScope
+function cl {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]
+        $_cd_paths
+    )
+    If ($null -ne $_cd_paths) {
+        _cd $_cd_paths
+    }
+    _ls
+}
+
+function c { cl @args; }
+### cl end ###
+
