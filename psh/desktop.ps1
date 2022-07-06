d9353fe77a220d2285efe2824b354035a6666c70
dqgthb <kimdhyungg@gmail.com>
2022-07-06 14:36:20 +0900

    Update desktop.ps1

--- psh/desktop.ps1
+++ psh/desktop.ps1
@@ -1,4 +1,6 @@
 # emacs keybinding such as ctrl-w
+$ErrorActionPreference = "Stop" # this will stop the script on error
+
 set-psreadlineoption -editmode vi
 
 function x() { python main.py }
@@ -17,60 +19,43 @@ function profiledir(){
     cd "$home\Documents\WindowsPowerShell\"
 }
 
-function alex() {
-    set-location "$home\Documents\Github\Alex"
-
+# No use. It requires admin previlege anyway...
+function ln ($target, $link) {
+    new-item -path $link -itemtype SymbolicLink -value $target
 }
 
 function dotfiles() {
     set-location "$home\Documents\Github\dotfiles"
 }
 
-function ls() {
-    get-childItem @args -force
-}
-
-function l() {
-    get-childItem @args -force
-}
-
-function c() {
-    set-location @args; get-childItem
-}
-
-$alexPath = "$home/Documents/github/Alex"
-
-function exe() {
-    set-location "$alexPath/out/build/x64-Debug/"
-}
-
 function diffBinary() {
     fc.exe @args;
 }
 
-# CG hw 2
+### CG hw 3 begin ###
 
 $githubPath = "$home/Documents/github"
 
 function cg() {
-    set-location "$githubPath/cg-hw2"
+    set-location "$githubPath/cg-hw3"
 }
-
 
 function hw() {
-    set-location "$githubPath/cg-hw2"
+    set-location "$githubPath/cg-hw3"
 }
 
-
-function ln ($target, $link) {
-    new-item -path $link -itemtype SymbolicLink -value $target
+function ppt() {
+    ii C:\Users\kimdh\Documents\ku4-1\cg\hw3.pptx
 }
+
+### CG hw 2 end ###
+
 
 ### cl begin ###
+
 function _ls{Get-ChildItem -name @args -Force}
 
 Set-Alias l _ls
-
 Set-Alias -Name ls -Value _ls -Option AllScope
 
 function ll{Get-ChildItem}
@@ -112,6 +97,7 @@ function cl {
 function c { cl @args; }
 ### cl end ###
 
+
 ### baekjoon begin ###
 function cpm {
     cat main.py | clip
@@ -135,7 +121,7 @@ function prepare($dir) {
         return
     }
     mkdir $dir;
-    copy-item -Path "$env:userprofile\Documents\Github\baekjoon\template\*" -Destination $dir -recurse
+    copy-item -Path "$env:userprofile\Documents\Github\algorithms\template\*" -Destination $dir -recurse
 }
 
 function play($dir) {
@@ -144,8 +130,8 @@ function play($dir) {
     prepare $dir
     cd $dir;
     code -r .
-    #code --goto main.py:23:4
-    code --goto main.cpp:70:4
+    code --goto main.py:23:4
+    #code --goto main.cpp:70:4
 }
 
 function compete($dir) {
@@ -163,11 +149,13 @@ function compete($dir) {
 
 
 ### zlocation, fzf and fd begin ###
+
 Import-Module ZLocation # this must come after other imports that modify prompt
 Write-Host -Foreground Green "`n[ZLocation] knows about $((Get-ZLocation).Keys.Count) locations.`n" # shows stat. must come after import zlocation
-
+# you can RESET database by deleting $home/z-location.db file.
 # Then, install fzf with 'sudo choco install fzf'
 function cz{ z | python -c "z=list(__import__('sys').stdin); z=[s.strip().split() for s in z]; z=[l[1] for l in z[3:-2]]; print('\n'.join(z))" | fzf | cl }
 # Then, 'sudo choco install fd'
 function cx{ fd --type d | fzf | cl }
+
 ### zlocation, fzf and fd end ###
