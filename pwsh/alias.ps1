21e01e33bee42940f0acbca5e0c503c43d474561
Daehyun Kim <kimdhyungg@gmail.com>
2023-01-18 20:05:20 +0900

    Create alias.ps1

---
+++ pwsh/alias.ps1
@@ -0,0 +1,11 @@
+function ali() {
+    code $home/documents/github/dotfiles/pwsh/alias.ps1
+}
+
+function spring_inflearn() {
+    start chrome https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%EC%9E%85%EB%AC%B8-%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8/unit/48553
+}
+
+function cs() { 
+    # start chrome ,q
+}
